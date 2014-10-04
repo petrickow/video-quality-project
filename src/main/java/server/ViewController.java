02c18b78ac31fe3ee20671117574d82d7de7926a
Michael Timpelan <lexxodu@gmail.com>
2014-10-04 17:02:08 +0200

    Request template-stream-view directive

--- src/main/java/server/ViewController.java
+++ src/main/java/server/ViewController.java
@@ -14,34 +14,41 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class ViewController {
-	
-	ArrayList<Thread> connections = new ArrayList<Thread>();
-	
-    @RequestMapping("/greeting") // for testing
-    public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
-        model.addAttribute("name", name);
-        Thread arrived = new Thread(new Worker());
-        arrived.start();
-        connections.add(arrived);
-        
-        return "greeting";
-    }
-    
-    @RequestMapping("/")
-    public String index() {
-    	
-    	return "index";
-    }
 
-    
-    class Worker implements Runnable {
+	ArrayList<Thread> connections = new ArrayList<Thread>();
+
+	@RequestMapping("/greeting")
+	// for testing
+	public String greeting(
+			@RequestParam(value = "name", required = false, defaultValue = "World") String name,
+			Model model) {
+		model.addAttribute("name", name);
+		Thread arrived = new Thread(new Worker());
+		arrived.start();
+		this.connections.add(arrived);
+
+		return "greeting";
+	}
+
+	@RequestMapping("/")
+	public String index() {
+
+		return "index";
+	}
+
+	@RequestMapping("/template-video-stream")
+	public String templateVideoStream() {
+		return "template-video-stream";
+	}
+
+	class Worker implements Runnable {
 
 		@Override
 		public void run() {
 			// do stuff
-			
-			System.out.println(connections.size());
+
+			System.out.println(ViewController.this.connections.size());
 		}
-    	
-    }
+
+	}
 }
