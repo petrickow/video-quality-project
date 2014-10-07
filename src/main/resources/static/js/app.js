5108404c089ffd3749e08bb5cc22239d84537c9b
agareis <aragara@gmx.de>
2014-10-07 11:30:56 +0200

    Initial Gmap implementation with random GPS data

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,4 +1,4 @@
-var app = angular.module('video-quality-manager', []);
+var app = angular.module('video-quality-manager', ['ngMap']);
 
 app.factory('Socket', [ '$rootScope', function ($rootScope) {
     "use strict";
@@ -36,7 +36,7 @@ app.factory('Socket', [ '$rootScope', function ($rootScope) {
                 console.log(event);
                 console.log(Service.index);
                 console.log(Service.data[Service.data.length - 1]);
-                } catch (e if e instanceof TypeError){
+                } catch (e){
                 console.log("Waiting for data...");
             }
          });
@@ -49,4 +49,36 @@ app.factory('Socket', [ '$rootScope', function ($rootScope) {
     
     return Service;
 }]);
+
+app.controller('mapController', 
+ function($scope, $http, $interval) {
+  var map;
+  $scope.$on('mapInitialized', function(e, evtMap) {
+    map = evtMap;
+  });
+
+  $scope.dynMarkers = [];
+  for (var i = 0; i < 8; i++) {
+    $scope.dynMarkers[i] = new google.maps.Marker({
+      title: "Marker: " + i
+    })
+  }
+
+  $scope.GenerateMapMarkers = function() {
+    var d = new Date(); //To show marker location changes over time
+    $scope.date = d.toLocaleString();
+
+    var numMarkers = Math.floor(Math.random() * 4) + 4; //between 4 to 8 markers
+    for (i = 0; i < numMarkers; i++) {
+      var lat = 49.4900000 + (Math.random() / 100); 
+      var lng = 8.4800300 + (Math.random() / 100);
+
+      var loc = new google.maps.LatLng(lat, lng);
+      $scope.dynMarkers[i].setPosition(loc);
+      $scope.dynMarkers[i].setMap(map);
+    }
+  };
+
+  $interval($scope.GenerateMapMarkers, 2000);
+});
                    
