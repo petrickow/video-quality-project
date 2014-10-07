62a3de29d93e9c82cb3f84ad8e8fd7169f9037bf
Cato Danielsen <catodanielsen@gmail.com>
2014-10-07 17:01:45 +0200

    Added logging for potentially easier debugging

--- src/main/java/server/WebsocketStreamReq.java
+++ src/main/java/server/WebsocketStreamReq.java
@@ -2,15 +2,17 @@ package server;
 
 import java.io.IOException;
 
+import org.apache.log4j.Logger;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
 public class WebsocketStreamReq extends TextWebSocketHandler {
-    @Override
+	private static Logger log = Logger.getLogger(WebsocketStreamReq.class.getName());
+
+	@Override
     public void handleTextMessage(WebSocketSession session, TextMessage message) throws IOException {
-        System.out.println(message);
+        log.debug(message);
         session.sendMessage(new TextMessage("You got it, now stop calling, I'll call you"));
-        
     }	
 }
